b41bba308504bba4207e253774e339074735d0d6
alvin <alvin@Mac.localdomain>
2025-08-19 15:38:50 -0700

    dracula theme working

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,28 @@
 return {
     -- add gruvbox
     --{ "ellisonleao/gruvbox.nvim" },
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    -- Configure LazyVim to load gruvbox
+    --{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    ---- Configure LazyVim to load gruvbox
+    --{
+    --    "LazyVim/LazyVim",
+    --    opts = {
+    --        --colorscheme = "gruvbox",
+    --        -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+    --        colorscheme = "catppuccin-macchiato",
+    --    },
+    --},
+    {
+        "Mofiqul/dracula.nvim",
+        config = function()
+            vim.cmd([[colorscheme dracula]])
+        end,
+    },
     {
         "LazyVim/LazyVim",
         opts = {
             --colorscheme = "gruvbox",
             -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-            colorscheme = "catppuccin-macchiato",
+            colorscheme = "dracula",
         },
     },
 }
